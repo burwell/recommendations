2f7ce59bd47939bd5a77558303288cf509598c6b
Burwell 2 <dayjp@dev-server.local>
2010-03-25 11:22:54 -0600

    a few fixes to make this plugin work in admin

--- recommendations_extension.rb
+++ recommendations_extension.rb
@@ -14,16 +14,6 @@ class RecommendationsExtension < Spree::Extension
     # make the useful fn(s) available in Products
     ProductsHelper.module_eval do
       include RecommendationsHelper        # for finding recommendations to suggest
-    end 
- 
-    # register Accessories product tab
-    Admin::BaseController.class_eval do
-      before_filter :add_recommendations_tab
-      
-      private
-      def add_recommendations_tab
-        @product_admin_tabs << {:name => t('recommendations'), :url => "selected_admin_product_recommendations_url"}
-      end
     end
     
   end
